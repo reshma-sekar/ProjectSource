c351b14a61934eee9fae04922c51cc7a4b372ced
ACER <reshma_star6@yahoo.in>
2019-05-13 12:02:39 +0530

    Added a new method m1

--- TestProject/src/com/src/TestClass.java
+++ TestProject/src/com/src/TestClass.java
@@ -1,6 +1,11 @@
 package com.src;
 
 public class TestClass {
+	
+	public void m1()
+	{
+		System.out.println("method1");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
